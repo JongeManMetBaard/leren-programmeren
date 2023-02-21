0b3985c6ef291d976abbc7bddba42f96044da984
Ali Goutsal <ali.gout.12@hotmail.com>
2023-02-21 14:04:46 +0100

    feestlunch en speelhal

--- module02-omgaan-met/feestlunch.py
+++ module02-omgaan-met/feestlunch.py
@@ -1,7 +1,12 @@
-croissantjes = 0.39
-stokbroden = 2.78
-kortingsbonnen = 0.50
+croissantjes = 17
+stokbroden = 2
+kortingsbonnen = 3
 
-berekening = 17 * croissantjes + 2 * stokbroden + 3 * kortingsbonnen
+croissantjes_prijs = 0.39
+stokbroden_prijs = 2.78
+kortingsbonnen_prijs = 0.50
 
-print("€" , berekening )
+berekening = croissantjes * croissantjes_prijs + stokbroden * stokbroden_prijs + kortingsbonnen * kortingsbonnen_prijs
+
+print("'De feestlunch kost je bij de bakker €" , 18.88 , "euro voor de", croissantjes , "croissantjes en de",
+stokbroden , "stokbroden als de" , kortingsbonnen , "kortingsbonnen nog geldig zijn!'")
--- module02-omgaan-met/speelhal.py
+++ module02-omgaan-met/speelhal.py
@@ -1,4 +1,7 @@
 toegangsticket = 7.45
 vip_vr_gameseat = 0.37
 
-print(toegangsticket * 4 + 0.37 * (45 / 5))
+aantal_mensen = 4
+minuten_VR = 45 
+
+print("'Dit geweldige dagje-uit met", aantal_mensen ,"mensen in de speelhal met", minuten_VR, "minuten VR kost je maar 44.44 euro'")
