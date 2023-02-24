eee898a1df8afbb56d61737d20913c449241bf95
Ali Goutsal <ali.gout.12@hotmail.com>
2023-02-24 13:17:51 +0100

    person.py

---
+++ module02-omgaan-met/person.py
@@ -0,0 +1,13 @@
+naam = input("Wat is uw naam? ")
+adres = input("Wat is uw adres? ")
+postcode = input("Wat is uw postcode? ")
+woonplaats = input("Wat is uw woonplaats? ")
+
+print(" -----------------------------------------")
+print("-----------")
+print("|   Naam         :  ", naam)
+print("|   Adres        :  ", adres)
+print("|   Postcode     :  ", postcode)
+print("|   Woonplaats   :  ", woonplaats)
+print(" -----------------------------------------")
+print("-----------")
