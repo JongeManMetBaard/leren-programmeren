768d28d8691796a4b5e5412949c12de36056eb0a
Ali Goutsal <ali.gout.12@hotmail.com>
2023-02-24 18:56:21 +0100

    bedrag in cent

--- module02-omgaan-met/feestlunch.py
+++ module02-omgaan-met/feestlunch.py
@@ -1,12 +1,12 @@
-croissantjes = 17
-stokbroden = 2
-kortingsbonnen = 3
+croissantjes = int(input("Hoeveel croissantjes wilt u? "))
+stokbroden = int(input("Hoeveel strokbroden wilt u? "))
+kortingsbonnen = int(input("Hoeveel kortingsbonnen heeft u? "))
 
-croissantjes_prijs = 0.39
-stokbroden_prijs = 2.78
-kortingsbonnen_prijs = 0.50
+croissantjes_prijs = int(input("Hoeveel kost de croissantjes? "))
+stokbroden_prijs = int(input("Hoeveel kost de stokbroden? "))
+kortingsbonnen_prijs = int(input("Hoeveel korting? "))
 
-berekening = croissantjes * croissantjes_prijs + stokbroden * stokbroden_prijs + kortingsbonnen * kortingsbonnen_prijs
+berekening = croissantjes * croissantjes_prijs + stokbroden * stokbroden_prijs - kortingsbonnen * kortingsbonnen_prijs
 
-print("'De feestlunch kost je bij de bakker €" , berekening , "euro voor de", croissantjes , "croissantjes en de",
+print("'De feestlunch kost je bij de bakker" , berekening , "cent voor de", croissantjes , "croissantjes en de",
 stokbroden , "stokbroden als de" , kortingsbonnen , "kortingsbonnen nog geldig zijn!'")
--- module02-omgaan-met/speelhal.py
+++ module02-omgaan-met/speelhal.py
@@ -1,10 +1,11 @@
-toegangsticket = 7.45
-vip_vr_gameseat = 0.37
+toegangsticket = int(input("Hoeveel kost de toegangsticket? "))
+vip_vr_gameseat = int(input("Hoeveel kost de VIP-VR-GameSeat? "))
 
-aantal_mensen = 4
-minuten_VR = 45
+aantal_mensen = int(input("Hoeveel mensen? "))
+minuten_VR = int(input("Voor hoeveel minuten in de VIP-VR-GameSeat? "))
 
-berekening = toegangsticket * aantal_mensen + vip_vr_gameseat * minuten_VR
+berekening = toegangsticket * aantal_mensen + vip_vr_gameseat * (minuten_VR / 5)
+bedrag_in_cent = int(berekening)
 
 print("'Dit geweldige dagje-uit met", aantal_mensen ,"mensen in de speelhal met", minuten_VR, "minuten VR kost je maar",
-berekening ,"euro'")
+bedrag_in_cent ,"cent'")
