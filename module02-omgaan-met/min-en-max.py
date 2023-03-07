458fa2ecf91248801b93a0fc50ce10a52891d07d
Ali Goutsal <ali.gout.12@hotmail.com>
2023-03-07 15:11:17 +0100

    input en if-statement

--- module02-omgaan-met/min-en-max.py
+++ module02-omgaan-met/min-en-max.py
@@ -1,10 +1,6 @@
-a = int(input("Typ het gewenste getal in"))
-b = int(input("Typ nogmaals het gewenste getal in"))
+a = int(input("Typ het gewenste getal in: "))
+b = int(input("Typ nogmaals het gewenste getal in: "))
 
 if a > b:
     max = a
-    print("'a is het grootste getal:' gevold door de waarde van max")
-
-elif a < b:
-    min = a
-    print("'a is het kleinste getal:' gevold door de waarde van min")
+    print("'a is het grootste getal:'", max)
