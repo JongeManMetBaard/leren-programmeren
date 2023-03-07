ada17513ffc099d8b5008868e55798f738e0145e
Ali Goutsal <ali.gout.12@hotmail.com>
2023-03-07 15:06:53 +0100

    elif-statement

--- module02-omgaan-met/min-en-max.py
+++ module02-omgaan-met/min-en-max.py
@@ -4,3 +4,7 @@ b = int(input("Typ nogmaals het gewenste getal in"))
 if a > b:
     max = a
     print("'a is het grootste getal:' gevold door de waarde van max")
+
+elif a < b:
+    min = a
+    print("'a is het kleinste getal:' gevold door de waarde van min")
