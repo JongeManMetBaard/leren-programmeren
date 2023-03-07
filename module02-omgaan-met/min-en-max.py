a548835acef6e9cffcad280bccdbcbd9412873ac
Ali Goutsal <ali.gout.12@hotmail.com>
2023-03-07 15:18:01 +0100

    Min en max

--- module02-omgaan-met/min-en-max.py
+++ module02-omgaan-met/min-en-max.py
@@ -3,11 +3,16 @@ b = int(input("Typ nogmaals het gewenste getal in: "))
 
 if a > b:
     max = a
+    min = b
     print("a is het grootste getal:", max)
 
 elif a < b:
     min = a
+    max = b
     print("a is het kleinste getal:", min)
 
-# else:
-#     print("a en b zijn even groot")
+else:
+    print("a en b zijn even groot")
+
+print("Het minimum is:", min)
+print("Het maximum is:", max)
