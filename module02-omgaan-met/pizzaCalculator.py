fcc7e5661e6ec67c84c11554ba8c30a67638fcff
Ali Goutsal <ali.gout.12@hotmail.com>
2023-06-12 11:49:31 +0200

    invoer voorzien van exception-handling

--- module02-omgaan-met/pizzaCalculator.py
+++ module02-omgaan-met/pizzaCalculator.py
@@ -5,18 +5,20 @@ small = 14.99
 medium = 16.49
 large = 19.99
 
-# keuze
-
-def getInteger():
+# vraag geldige getalwaarde aan gebruiker
+def getInteger(amount):
     while True:
         try:
-            small_pizza = getInteger(int(input("Hoeveel small pizza's wilt u? ")))
-            medium_pizza = getInteger(int(input("Hoeveel medium pizza's wilt u? ")))
-            large_pizza = int(input("Hoeveel large pizza's wilt u? "))
-            break
+            aantal = int(input(amount))  
+            return aantal
         except ValueError:
             print("Dat is geen nummer")
 
+# keuze
+small_pizza = getInteger("Hoeveel small pizza's wilt u? ")
+medium_pizza = getInteger("Hoeveel medium pizza's wilt u? ")
+large_pizza = getInteger("Hoeveel large pizza's wilt u? ")
+    
 # berekening totaal prijs
 small_total_price = small_pizza * small
 medium_total_price = medium_pizza * medium
@@ -24,6 +26,7 @@ large_total_price = large_pizza * large
 
 # Totale kosten bestelling
 total_price_order = small_total_price + medium_total_price + large_total_price
+formatted_number = f"{total_price_order:.2f}"
 
 # bon
 print("+------------------+------------------+")
@@ -33,5 +36,5 @@ print("|      Small       |       ",   small_pizza,"        |")
 print("|      Medium      |       ",  medium_pizza,"        |")
 print("|      Large       |       ",   large_pizza,"        |")
 print("+------------------+------------------+")
-print("|   Totale kosten  |     €",   total_price_order,"     |")
+print("|   Totale kosten  |     €",   formatted_number,"     |")
 print("+------------------+------------------+")
--- module03-keer-op-keer/kladblaadje.py
+++ module03-keer-op-keer/kladblaadje.py
@@ -1,7 +1,51 @@
-while True:
-    try:
-        getal = int(input("voer een getal in: "))
-    except ValueError:
-        print("voer een getal in sukkelaar")
-    else:
-        break
+# while True:
+#     try:
+#         getal = int(input("voer een getal in: "))
+#     except ValueError:
+#         print("voer een getal in sukkelaar")
+#     else:
+#         break
+
+# number = 2.705
+# formatted_number = f"{number:.2f}"
+# print(formatted_number)  # Output: 2.70
+
+# Ali Goutsal pizzaCalculator
+
+# prijzen
+small = 14.99
+medium = 16.49
+large = 19.99
+
+# functie om een geldig geheel getal te verkrijgen van de gebruiker
+def getIntegerInput(prompt):
+    while True:
+        try:
+            value = int(input(prompt))
+            return value
+        except ValueError:
+            print("Dat is geen geldig geheel getal. Probeer het opnieuw.")
+
+# keuze
+small_pizza = getIntegerInput("Hoeveel small pizza's wilt u? ")
+medium_pizza = getIntegerInput("Hoeveel medium pizza's wilt u? ")
+large_pizza = getIntegerInput("Hoeveel large pizza's wilt u? ")
+
+# berekening totaal prijs
+small_total_price = small_pizza * small
+medium_total_price = medium_pizza * medium
+large_total_price = large_pizza * large
+
+# Totale kosten bestelling
+total_price_order = small_total_price + medium_total_price + large_total_price
+
+# bon
+print("+------------------+------------------+")
+print("|     Afmeting     |      Aantal      |")
+print("+------------------+------------------+")
+print("|      Small       |      {:>4d}        |".format(small_pizza))
+print("|      Medium      |      {:>4d}        |".format(medium_pizza))
+print("|      Large       |      {:>4d}        |".format(large_pizza))
+print("+------------------+------------------+")
+print("|   Totale kosten  |   € {:>7.2f}   |".format(total_price_order))
+print("+------------------+------------------+")
